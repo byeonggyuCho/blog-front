3ecbf05d90a23fd284a31d96dce69cff7d06137e
CaterJo <kin7729@naver.com>
2020-04-02 15:10:30 +0900

    Edit/ redux store configure

--- src/store/configure.ts
+++ src/store/configure.ts
@@ -1,34 +1,23 @@
-
-/**
- *  리덕스 스토어 설정
- * 
- */
 import {createStore, applyMiddleware } from 'redux';
-import penderMiddleware from 'redux-pender';
 import createSagaMiddleware from 'redux-saga';
-// import creat, { rootSaga } from 'reducers';
 import createRootReducer from 'reducers';
 import rootSaga from 'sagas'
 import { composeWithDevTools } from 'redux-devtools-extension';
-
+import { routerMiddleware as createRouterMiddleware} from "connected-react-router";
 import { createBrowserHistory } from 'history'
+/**
+ *  리덕스 스토어 설정
+ */
 
 
 export const history = createBrowserHistory() 
-
-
-// const reducers = combineReducers(modules);
-
 const sagaMiddleware = createSagaMiddleware();
-const pendderMiddleware = penderMiddleware()
-const middlewares = [ pendderMiddleware, sagaMiddleware ];
-
+const routerMiddleware = createRouterMiddleware(history);
 
 // 개발 모드 일 때만 Redux Devtools를 적용합니다..
 // const isDev = process.env.NODE_ENV === 'development';
 // const devtools = isDev && window.__REDUX_DEVTOOLS_EXETENSION_COMPOSE__;
 // const composeEnhancers = devtools || compose;
-
 
 
 // preloadedState는 추후 서버사이드 랜더링을 했을 때 전달받는 초기 상태이다.
@@ -39,7 +28,10 @@ export default function configureStore (preloadedSate?)  {
         createRootReducer(history), // root reducer with router state
         preloadedSate, 
         composeWithDevTools(
-            applyMiddleware(...middlewares)
+            applyMiddleware(
+                sagaMiddleware,
+                routerMiddleware
+            )
         )
     );
 
